45c7b0d5b7b39a2158ad557d0a675fab9716dd8c
phouk-sophean <phouksophean36@gmail.com>
2025-05-20 14:59:23 +0700

    done teacher feature

---
+++ src/Teacher.ts
@@ -0,0 +1,13 @@
+class Teacher {
+    specialization: string;
+    subject: Subject[];
+
+    constructor(specialization: string) {
+        this.specialization = specialization;
+        this.subject = [];
+    }
+
+    public assignSubject(){
+        return this.assignSubject;
+    }
+}
