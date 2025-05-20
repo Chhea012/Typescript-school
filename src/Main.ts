bdd8f8199da67cf76cfeddabc7fd6beb18974f4f
Sokha Sim <sokha.sim@student.passerellesnumeriques.org>
2025-05-20 15:25:00 +0700

    school

--- src/Main.ts
+++ src/Main.ts
@@ -1,8 +1,2 @@
-import { Classroom } from "./Classroom";
-import { Student } from "./Student";
-import { Teacher } from "./Teacher";
-import { School } from "./School";
-import { Subject } from "./Subject";
-
 
 
